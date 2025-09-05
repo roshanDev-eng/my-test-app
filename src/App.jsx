712720bcc3cd2dea7b015f9288e04abf3e068aaf
Murtaza <alimurtazabhatti0307@gmail.com>
2025-09-05 19:38:36 +0500

    .....

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css"; // <-- import the CSS file
 
 const App = () => {
-  return <div className="hello">hello saliha</div>;
+  return <div className="hello">..........</div>;
 };
 
 export default App;
